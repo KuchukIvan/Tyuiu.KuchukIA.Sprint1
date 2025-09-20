60693aa5d3f2b87149b0fed97c76efb211132ce9
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-09-20 23:51:45 +0500

    Task3

---
+++ Tyuiu.KuchukIA.Sprint1.Task3.V12.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.KuchukIA.Sprint1.Task3.V12.Lib;
+namespace Tyuiu.KuchukIA.Sprint1.Task3.V12.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double kat1 = 3;
+            double kat2 = 7;
+            var res = ds.TriangleArea(kat1, kat2);
+            Assert.AreEqual(10.5, res);
+        }
+    }
+}
---
+++ Tyuiu.KuchukIA.Sprint1.Task3.V12/Program.cs
@@ -0,0 +1,41 @@
+﻿using Tyuiu.KuchukIA.Sprint1.Task3.V12.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #1 | Выполнил: И.А. Кучук | ПИНб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #1                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #12                                                              *");
+        Console.WriteLine("* Выполнил: Кучук Иван Александрович |  ПИНб-25-1                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу для вычисления площади прямоугольного                *");
+        Console.WriteLine("* треугольника по длинам двух катетов.                                    *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        double kat1;
+        double kat2;
+
+        Console.WriteLine("Введите значение первого катета: ");
+        kat1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите значение второго катета: ");
+        kat2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Площадь треугольника равна: " + ds.TriangleArea(kat1, kat2));
+
+        Console.ReadKey();
+
+    }
+}
