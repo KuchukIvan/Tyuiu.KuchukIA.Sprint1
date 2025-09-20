cd3e5277f584fc53216d4e5bfbb7f181bde20f70
Иван Кучук <Tyuiu.KuchukIA@outlook.com>
2025-09-20 23:03:35 +0500

    Tasl2

---
+++ Tyuiu.KuchukIA.Sprint1.Task2.V14.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.KuchukIA.Sprint1.Task2.V14.Lib;
+namespace Tyuiu.KuchukIA.Sprint1.Task2.V14.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 400;
+            var res = ds.ConvertKelvinToCelsius(x);
+            Assert.AreEqual(126, res);
+        }
+    }
+}
--- Tyuiu.KuchukIA.Sprint1.Task2.V14/Program.cs
+++ Tyuiu.KuchukIA.Sprint1.Task2.V14/Program.cs
@@ -1,4 +1,4 @@
-﻿using Tyuiu.KuchukIA.Sprint1.Task1.V12.Lib;
+﻿using Tyuiu.KuchukIA.Sprint1.Task2.V14.Lib;
 internal class Program
 {
     static void Main(string[] args)
@@ -8,33 +8,28 @@ internal class Program
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* Спринт #1                                                               *");
         Console.WriteLine("* Тема: Базовые навыки работы в C#                                        *");
-        Console.WriteLine("* Задание #1                                                              *");
-        Console.WriteLine("* Вариант #12                                                              *");
+        Console.WriteLine("* Задание #2                                                              *");
+        Console.WriteLine("* Вариант #14                                                              *");
         Console.WriteLine("* Выполнил: Кучук Иван Александрович |  ПИНб-25-1                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
-        Console.WriteLine("* Написать консольную программу на C#, которая запрашивает у пользователя *");
-        Console.WriteLine("* исходные данные, вычисляет результат по формуле  (x+y)/6                *");
-        Console.WriteLine("* и печатает его на экране.                                               *");
+        Console.WriteLine("* Известна температура в градусах Кельвина.                               *");
+        Console.WriteLine("* Перевести температуру в градусы Цельсия.                                *");
         Console.WriteLine("*                                                                         *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
 
-        double x, y;
+        int x;
         Console.WriteLine("Введите значение x:");
-        x = Convert.ToDouble(Console.ReadLine());
-
-        Console.WriteLine("Введите значение y:");
-        y = Convert.ToDouble(Console.ReadLine());
+        x = Convert.ToInt32(Console.ReadLine());
 
         Console.WriteLine();
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
-
-        Console.WriteLine(ds.Calculate(x, y));
+        Console.WriteLine("Температура в градусах Цельсия равна: " + ds.ConvertKelvinToCelsius(x));
         Console.ReadKey();
-     
+
     }
 }
